f783995dfc53574288fcf0922d601f50924ca9a5
Vadym Zelinskyi <vadym.zelinskyi@rubyplay.com>
2025-07-24 16:46:46 +0200

    Fixed config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
-
 module.exports = {
     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     USER_ID: 367724841,
     prompt: `
+Згенеруй список із 10 англійських слів рівня Intermediate для вивчення. Для кожного слова вкажи:
+- слово та транскрипцію у квадратних дужках (наприклад: example [ɪgˈzɑːmpəl])
+- переклад українською
+- 1 приклад речення
+
+Формат:
+
+1. слово [транскрипція] — переклад
+Example: приклад речення англійською
 `,
     extraKeyboard: {
         reply_markup: {
